20ba6b9d351cd63c79b89d6a03e3c211a079e145
HiHaker <1434938035@qq.com>
2021-12-08 19:50:04 +0800

    目录结构优化

--- 实战/GAN实战/GAN/test.py
+++ 实战/GAN实战/GAN/test.py
@@ -1,12 +1,28 @@
-import torch
-import torch.nn as nn
+# import torch
+# print(torch.cuda.is_available())
+# print(torch.cuda.device_count())
+# print(torch.cuda.current_device())
+# # GPU
+# torch.cuda.set_device(3)
+# print(torch.cuda.current_device())
 
-m = nn.Sigmoid()
-loss = nn.BCELoss()
-input = torch.randn(3, requires_grad=True)
-print(input)
-target = torch.empty(3).random_(2)
-print(target)
-print(m(input))
-output = loss(m(input), target)
-output.backward()
+# import pickle
+#
+# lossD = [1, 2.0]
+# lossG = [2.0, 1]
+#
+# with open('./data/lossD.txt', 'wb') as f:
+#     pickle.dump(lossD, f)
+#
+# with open('./data/lossG.txt', 'wb') as f:
+#     pickle.dump(lossG, f)
+#
+# with open('./data/lossD.txt', 'rb') as f:
+#     D = pickle.load(f)
+#
+# with open('./data/lossG.txt', 'rb') as f:
+#     G = pickle.load(f)
+#
+# print(D)
+# print(G)
+
--- 实战/GAN实战/GAN/train.py
+++ 实战/GAN实战/GAN/train.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import pickle
 from torchvision import datasets
 import torchvision.transforms as transforms
 from torch.utils.data.dataset import Dataset
@@ -9,7 +10,10 @@ from model import Generator, Discriminator
 # 批大小
 batch_size = 32
 # 线程数
-num_workers = 0
+num_workers = 4
+# GPU
+device = torch.device("cuda:3" if torch.cuda.is_available() else "cpu")
+
 
 # ToTensor()将shape为(H, W, C)的nump.ndarray或img转为shape为(C, H, W)的tensor，其将每一个数值归一化到[0,1]
 transform = transforms.ToTensor()
@@ -35,7 +39,9 @@ optimizerG = optim.SGD(G.parameters(), lr=learning_rate)
 optimizerD = optim.SGD(D.parameters(), lr=learning_rate)
 
 # 训练轮次
-epoch = 20
+epoch = 100
+total_lossD = []
+total_lossG = []
 for i in range(epoch):
     # 训练
     for step, data in enumerate(train_loader, start=1):
@@ -55,11 +61,15 @@ for i in range(epoch):
         # 图片打平
         images = torch.flatten(images, start_dim=1)
         fake_img = G(noise)
+
         # Generator 产生的数据的标签为0
         fake_label = torch.zeros([batch_size])
         imgs_label = torch.ones([batch_size])
+
+        # concat
         total_img = torch.cat((fake_img, images), dim=0)
         total_label = torch.cat((fake_label, imgs_label), dim=0)
+
         outputs = D(total_img)
         outputs = torch.flatten(outputs)
         lossD = criterion(outputs, total_label)
@@ -69,13 +79,25 @@ for i in range(epoch):
         # 固定住D，训练G
         D.eval()
         G.train()
+
         # 产生高斯噪声数据
         noise = torch.normal(0, 1, (batch_size, 10))
+
         fake_img = G(noise)
         outputs = D(fake_img)
         outputs = torch.flatten(outputs, start_dim=1)
-        lossG = 1-torch.log(outputs).sum()
+        lossG = -torch.log(outputs).sum()
         lossG.backward()
         optimizerG.step()
 
+    total_lossD.append(lossD.item())
+    total_lossG.append(lossG.item())
     print('epoch: %d--lossD: %.3f, lossG: %.3f.' % (i, lossD, lossG))
+    # 每隔20轮保存一下模型
+    if i+1 % 20 == 0:
+        torch.save(G, './model/G{}.pth'.format(i+1))
+        torch.save(D, './model/D{}.pth'.format(i+1))
+with open('./data/lossD.txt', 'wb') as f:
+    pickle.dump(D, f)
+with open('./data/lossG.txt', 'wb') as f:
+    pickle.dump(G, f)
